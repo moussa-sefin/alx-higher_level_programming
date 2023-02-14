b9d8311376cef1f177f947d5ca2d10f9422f6888
root <root@DESKTOP-V9A6DS0.localdomain>
2023-02-14 14:57:53 -0800

    review task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,6 @@
 -- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-mysql -u root -p hbtn_0c_0
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+mysql -hlocalhost -uroot -p hbtn_0c_0 <<EOF
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School');
+EOF
 
