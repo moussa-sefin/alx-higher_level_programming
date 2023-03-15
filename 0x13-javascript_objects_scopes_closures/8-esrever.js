d17b68991bf08c20cfb433a6abf6bfb8c3ed86f2
moussa-sefin <yahmoussa5@gmail.com>
2023-03-15 16:22:31 -0400

    task8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,6 +2,9 @@
 // function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-  const rlist = list.slice(-1);
-  return rlist;
+  const reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
 };
