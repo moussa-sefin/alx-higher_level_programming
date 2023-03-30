cc81af4bf1a50a7f960295395d188b168b02432e
root <root@DESKTOP-V9A6DS0.localdomain>
2023-03-31 01:14:51 +0200

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request and displays the size of the body of the response
-curl -sI $1 | awk '/Content-Length/ {print $2}' | tr -d '\r                                                      
+curl -sI $1 | awk '/Content-Length/ {print $2}' | tr -d '\r'                                                     
