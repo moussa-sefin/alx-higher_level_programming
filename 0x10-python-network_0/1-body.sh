4e7935aa1f2d27b28edf616956e844b87449f639
root <root@DESKTOP-V9A6DS0.localdomain>
2023-03-31 02:09:38 +0200

    bath push

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# This script takes in a URL, sends GET request and displays body of response
-curl -sL "$1"
+# This script takes a URL as an argument, sends a GET request to the URL using curl, and displays the body of the response if the status code is 200
+
+curl -sLI $1 -o /dev/null -w '%{http_code}\n' | grep -q 200 && curl -sL $1
